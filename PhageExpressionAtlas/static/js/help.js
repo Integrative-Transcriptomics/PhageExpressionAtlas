0503ef95457d6595f072620b289475bf7ea06928
Caroline <131356373+caro-cmd@users.noreply.github.com>
2025-05-02 16:24:25 +0200

    Added more to the guide

--- PhageExpressionAtlas/static/js/help.js
+++ PhageExpressionAtlas/static/js/help.js
@@ -31,5 +31,73 @@ export function initializeHelpPage(){
       }
     })
 
+    // add a popup for images, that if clicked the same image opens in large
+    const images = document.querySelectorAll("img");
+
+    // loop through the images
+    images.forEach(img => {
+
+      // on click create popup, popup wrapper (div that spans the whole screen), close button and image
+      img.onclick = function() {
+
+        // create popup and wrapper
+        const popup = document.createElement("div");
+        popup.classList.add("img-viewer");
+        const popup_wrapper = document.createElement("div");
+        popup_wrapper.classList.add("popup-wrapper");
+
+        // create close button
+        const close_btn = document.createElement("sl-icon-button");
+        close_btn.name= "x";
+        close_btn.label = "Close";
+
+        // add close button function
+        close_btn.onclick = function(){
+          popup_wrapper.remove(); // remove popup wrapper after clicking close button
+        }
+
+        // create big image
+        const big_img = document.createElement("img");
+        big_img.src = img.src;
+        big_img.classList.add("big-img");
+
+        // append close button and big image to the popup
+        popup.appendChild(close_btn);
+        popup.appendChild(big_img);
+
+        // append popup to popup wrapper
+        popup_wrapper.appendChild(popup);
+        
+        // handle clicks outside of the popup so that it closes/is removed if the popup wrapper but not the popup is clicked
+        popup_wrapper.addEventListener('click', (event) => {
+          if (!popup.contains(event.target)) {
+            popup_wrapper.remove(); 
+          }
+        });
+
+        // append popup wrapper to the body
+        document.body.appendChild(popup_wrapper);
+
+      }
+    })
+
+    // do the same with sl-animated images
+    const animated_images = document.querySelectorAll("sl-animated-image");
+
+    animated_images.forEach(img => {
+      
+      console.log(img.shadowRoot.querySelector("div").shadowRoot)
+
+      const controlBox = img.shadowRoot.querySelector('.animated-image__control-box');
+
+      
+
+      img.addEventListener('mousemove', (e) => {
+        const isOverControlBox = controlBox.contains(e.target);
+        img.style.cursor = isOverControlBox ? 'default' : 'zoom-in';
+      });
+
+    })
+
 
 }
